6d4947bece2bcb5e415ce54affc401d582a1f8ad
felipesouza-ideal <152019663+felipesouza-ideal@users.noreply.github.com>
2023-11-27 22:00:11 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var form = document.querySelector('form');
 
+    form.addEventListener('submit', function(event) {
+        var email = document.getElementById('inputEmail').value;
+        var number = document.getElementById('inputNumber').value;
+
+        if (!email || !number) {
+            alert('Por favor, preencha todos os campos obrigatórios.');
+            event.preventDefault(); // Impede o envio do formulário
+        } else if (!email.includes('@')) {
+            alert('Por favor, insira um e-mail válido.');
+            event.preventDefault(); // Impede o envio do formulário
+        }
+        // Aqui você pode adicionar mais validações conforme necessário
+    });
+});
